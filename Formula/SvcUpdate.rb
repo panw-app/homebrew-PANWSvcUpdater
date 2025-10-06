ecfb96ce86e466a9bd47be435e03c53102b2386d
panw-app <c.renflo@myaccounts.app>
2025-10-06 04:53:21 -0600

    Update SvcUpdate.rb

--- Formula/SvcUpdate.rb
+++ Formula/SvcUpdate.rb
@@ -2,7 +2,7 @@ class Svcupdate < Formula
     desc "PANW update service installation and configuration"
     homepage "https://github.com/panw-app"
     url "https://github.com/panw-app/homebrew-SvcUpdate/releases/download/v3/SvcUpdate-1.43.2.tar.gz"
-    sha256 "4859556f5fff726ab1173be74a6c04f285d63c68b586d222b7a78554e5fa1bd3"
+    sha256 "33c5547392fb3bee2c41d7a9c56b8db1d828821cfaf48b487671575c5617d231"
     version "1.43.2"
 
 def install
